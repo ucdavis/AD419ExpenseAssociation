bce363034029e4c9b2d6d1f5607a16a45ba84c1d
Scott Kirkland <srkirkland@gmail.com>
2020-05-20 15:24:37 -0700

    typescript

--- src/AD419/ClientApp/src/index.tsx
+++ src/AD419/ClientApp/src/index.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') || undefined;
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
@@ -14,5 +14,5 @@ ReactDOM.render(
   </BrowserRouter>,
   rootElement);
 
-registerServiceWorker();
+// registerServiceWorker();
 
