5fdc8dff931dedceb68d9e8342225bc20071078a
Scott Kirkland <srkirkland@gmail.com>
2020-05-21 10:34:21 -0700

    some basic org fetching

--- src/AD419/ClientApp/src/components/associations/AssociationContainer.tsx
+++ src/AD419/ClientApp/src/components/associations/AssociationContainer.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ExpenseRecordsContainer from "./ExpenseRecordsContainer";
 import ProjectsContainer from "./ProjectsContainer";
 
+import { Organization } from "../../models";
+
 export default function AssociationContainer(): JSX.Element {
+  const [orgs, setOrgs] = useState<Organization[]>([]);
+  const [selectedOrg, setSelectedOrg] = useState<Organization>();
+
+  // fire only once to grab initial orgs
+  useEffect(() => {
+    const getDepartments = async (): Promise<void> => {
+      const result = await fetch("/Organization");
+      const data = await result.json() as Organization[];
+
+      if (data && data.length > 0) {
+        setOrgs(data);
+        setSelectedOrg(data[0]);
+      }
+    };
+
+    getDepartments(); // go grab the depts
+  }, []);
+
+  const orgSelected = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const val = e.target.value;
+
+    const org = orgs.find(o => o.code === val);
+    setSelectedOrg(org);
+  };
+
   return (
     <div className="row">
+      <select name="orgs" onChange={orgSelected}>
+        {orgs.map((org) => (
+          <option key={org.code} value={org.code}>
+            {org.name}
+          </option>
+        ))}
+      </select>
+      {selectedOrg && selectedOrg.name}
       <div className="col-sm">
         <ExpenseRecordsContainer></ExpenseRecordsContainer>
       </div>
---
+++ src/AD419/Controllers/OrganizationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AD419.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+        private static readonly Organization[] Orgs = new[]
+        {
+            new Organization { OrgR = "AANS", OrgR_Dept = "AANS" },
+            new Organization { OrgR = "ABAE", OrgR_Dept = "ABAE" },
+        };
+
+        [HttpGet]
+        public IEnumerable<Organization> Get()
+        {
+            return Orgs;
+        }
+    }
+
+    public class Organization {
+        public string OrgR { get; set; }
+        public string OrgR_Dept { get; set; }
+    }
+}
