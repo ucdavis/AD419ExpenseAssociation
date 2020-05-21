1930a958d505f0102c9295f6829afa0ffc02cfba
Scott Kirkland <srkirkland@gmail.com>
2020-05-21 14:58:33 -0700

    not bad

--- src/AD419/Controllers/ExpenseController.cs
+++ src/AD419/Controllers/ExpenseController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using AD419.Services;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AD419.Controllers
@@ -7,16 +10,29 @@ namespace AD419.Controllers
     [Route("[controller]")]
     public class ExpenseController : ControllerBase
     {
+        private readonly IDbService _dbService;
+
+        public ExpenseController(IDbService dbService)
+        {
+            this._dbService = dbService;
+        }
         private static readonly Expense[] Expenses = new[]
         {
             new Expense { Chart = "3", Code = "AAKH", Description = "Startup Funds", Spent = 11872.23m, FTE = 0.75m, Num = 12, IsAssociated = false },
             new Expense { Chart = "3", Code = "ABCD", Description = "Project Funds", Spent = 22334.23m, FTE = 0.37m,  Num = 5, IsAssociated = true },
         };
 
+
         [HttpGet]
         public IEnumerable<Expense> Get(string org, string grouping = "Organization", bool showAssociated = false, bool showUnassociated = true)
         {
-            return Expenses;
+            using (var conn = _dbService.GetConnection()) {
+                return conn.Query<Expense>("usp_getExpenseRecordGrouping", 
+                new { Grouping = "Organization", OrgR = "AANS", Associated = true, Unassociated = true },
+                commandType: CommandType.StoredProcedure);
+            }
+            
+            // return Expenses;
         }
     }
 
