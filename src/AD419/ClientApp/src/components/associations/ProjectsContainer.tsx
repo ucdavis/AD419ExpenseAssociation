95b75f1721cd58ee6d3d2eab14e67d933c1f5b00
Scott Kirkland <srkirkland@gmail.com>
2020-05-27 13:47:31 -0700

    handle unselect as well

--- src/AD419/ClientApp/src/components/associations/ProjectsContainer.tsx
+++ src/AD419/ClientApp/src/components/associations/ProjectsContainer.tsx
@@ -11,7 +11,9 @@ interface Props {
 
 export default function ProjectsContainer(props: Props): JSX.Element {
   const [projects, setProjects] = useState<Project[]>([]);
-  const [selectedAssociations, setSelectedAssociations] = useState<Association[]>([]);
+  const [selectedAssociations, setSelectedAssociations] = useState<
+    Association[]
+  >([]);
 
   useEffect(() => {
     const getProjects = async (): Promise<void> => {
@@ -32,12 +34,12 @@ export default function ProjectsContainer(props: Props): JSX.Element {
 
     // first we need to figure out each project's association percentage
     const totalAssociated = props.associations.reduce((sum, curr) => {
-      return sum + curr.spent
+      return sum + curr.spent;
     }, 0);
 
-    const associationsWithPercentages = props.associations.map(assoc => ({
+    const associationsWithPercentages = props.associations.map((assoc) => ({
       ...assoc,
-      percent: (assoc.spent / totalAssociated) * 100.0
+      percent: (assoc.spent / totalAssociated) * 100.0,
     }));
 
     setSelectedAssociations(associationsWithPercentages);
@@ -47,50 +49,62 @@ export default function ProjectsContainer(props: Props): JSX.Element {
     project: Project,
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
-    console.log('selected project', project);
-
     const { checked } = event.target;
+
+    let newAssociations: Association[] = [];
 
     if (checked) {
       // TODO: FUTURE FEATURE: once a percentage has been manually changed, don't change existing percentages
 
-      // new association desired, add to the list and then distribute percentages equally across all associations
-      const newAssociation: Association = { project: project.project, accession: project.accession, percent: 0, spent: 0, fte: 0 };
-      const newAssociations = [...selectedAssociations, newAssociation];
-
-      // amount each row should have with an equal distribution
-      const evenPercentage = (100.0 / newAssociations.length);
-      
-      setSelectedAssociations(newAssociations.map(assoc => ({
-        ...assoc,
-        percent: evenPercentage
-      })));
+      // new association desired, add to the list
+      const newAssociation: Association = {
+        project: project.project,
+        accession: project.accession,
+        percent: 0,
+        spent: 0,
+        fte: 0,
+      };
+      newAssociations = [...selectedAssociations, newAssociation];
     } else {
-      setSelectedAssociations([
+      newAssociations = [
         ...selectedAssociations.filter(
-          (assoc) =>
-            !(
-              assoc.project === project.project
-            )
+          (assoc) => !(assoc.project === project.project)
         ),
-      ]);
+      ];
     }
+
+    // amount each row should have with an equal distribution
+    const evenPercentage = 100.0 / newAssociations.length;
+
+    setSelectedAssociations(
+      newAssociations.map((assoc) => ({
+        ...assoc,
+        percent: evenPercentage,
+      }))
+    );
   };
 
   const isSelected = (project: Project): boolean => {
-    return selectedAssociations.some((assoc) => assoc.project === project.project);
+    return selectedAssociations.some(
+      (assoc) => assoc.project === project.project
+    );
   };
 
   // return the percentage associated with this project
   const associationPercentage = (project: Project): number | undefined => {
-    return selectedAssociations.find(assoc => assoc.project === project.project)?.percent
-  }
+    return selectedAssociations.find(
+      (assoc) => assoc.project === project.project
+    )?.percent;
+  };
 
   return (
     <div>
       <h1>Projects</h1>
       <p>Save buttons go here</p>
-      <button className='btn btn-primary' onClick={(): Promise<void> => props.associate(selectedAssociations)}>
+      <button
+        className='btn btn-primary'
+        onClick={(): Promise<void> => props.associate(selectedAssociations)}
+      >
         Assign
       </button>
       <button className='btn btn-warning' onClick={props.unassociate}>
