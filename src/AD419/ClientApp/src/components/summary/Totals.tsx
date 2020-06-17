8e4d0b4d5aea050621b2668de2eeea377b787c51
Scott Kirkland <srkirkland@gmail.com>
2020-06-17 10:39:58 -0700

    format the summary totals number vals

--- src/AD419/ClientApp/src/components/summary/Totals.tsx
+++ src/AD419/ClientApp/src/components/summary/Totals.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Organization, AssociationTotal } from '../../models';
+import NumberDisplay from '../NumberDisplay';
 
 interface Props {
   org: Organization | undefined;
@@ -25,27 +26,39 @@ export default function Totals(props: Props): JSX.Element {
   }, [org]);
 
   return (
-    <div className="card">
-    <table className='table'>
-      <thead>
-        <tr>
-          <th></th>
-          <th>SPENT</th>
-          <th>FTE</th>
-          <th>RECS</th>
-        </tr>
-      </thead>
-      <tbody>
-        {totals.map((total) => (
-          <tr key={total.name}>
-            <td>{total.name}</td>
-            <td>{total.spent}</td>
-            <td>{total.fte}</td>
-            <td>{total.recs}</td>
+    <div className='card'>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th></th>
+            <th>SPENT</th>
+            <th>FTE</th>
+            <th>RECS</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {totals.map((total) => (
+            <tr key={total.name}>
+              <td>{total.name}</td>
+              <td>
+                <NumberDisplay
+                  value={total.spent}
+                  precision={2}
+                  type='currency'
+                ></NumberDisplay>
+              </td>
+              <td>
+                <NumberDisplay
+                  value={total.fte}
+                  precision={4}
+                  type='number'
+                ></NumberDisplay>
+              </td>
+              <td>{total.recs}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
