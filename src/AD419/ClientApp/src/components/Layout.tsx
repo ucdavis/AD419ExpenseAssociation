38b5658f9352e220ce8fc8cda1f5d9e832b5eca9
Scott Kirkland <srkirkland@gmail.com>
2020-06-25 14:20:24 -0700

    show smart year in footer

--- src/AD419/ClientApp/src/components/Layout.tsx
+++ src/AD419/ClientApp/src/components/Layout.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
-export const Layout = (props: React.PropsWithChildren<{}>): JSX.Element => (<div>
-  <NavMenu />
-  <Container>
-    {props.children}
-  </Container>
-  <footer className="container text-center">
-  <p className="discreet">&copy; Copyright 2000-2021
-        The Regents of the University of California, Davis campus. All rights reserved.
-    </p>
-  </footer>
-</div>)
+export const Layout = (props: React.PropsWithChildren<{}>): JSX.Element => (
+         <div>
+           <NavMenu />
+           <Container>{props.children}</Container>
+           <footer className='container text-center'>
+             <p className='discreet'>
+               &copy; Copyright 2000-{new Date().getFullYear()}
+               The Regents of the University of California, Davis campus. All
+               rights reserved.
+             </p>
+           </footer>
+         </div>
+       );
