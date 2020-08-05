c21b0e40cfad446dcbe25525528998f03d6f20a8
Cal <cydoval@mac.com>
2020-08-05 15:45:00 -0600

    if else

--- src/AD419/ClientApp/src/components/associations/ExpensesEmpty.tsx
+++ src/AD419/ClientApp/src/components/associations/ExpensesEmpty.tsx
@@ -8,11 +8,18 @@ interface Props {
 export default function ExpensesEmpty(props: Props): JSX.Element {
   // Cal -- this being shown depends on what is selected in the expense grouping (show associated only, unassoc only, or both)
   // so we should customize the message for each or pick a generic one.  I pass in the grouping in the props is case we want to use it
+  if (true) {
   return (
-    <div className='text-center'>
-      <h2>It looks like there are no unassociated items in here!</h2>
-      <p>Perhaps it's time for icecream?</p>
-      <i className='fas fa-ice-cream'></i>
+      <div>
+      <p className="text-center pt-2">Looks like there are no more unassociated items in this department <br/>
+      Maybe it's time for an ice cream?
+      </p>
     </div>
-  );
+    ); } 
+    
+    else { return (
+      <div>
+      <p className="text-center mt-2">There is nothing to display with these parameters</p>
+    </div>
+    ) }
 }
