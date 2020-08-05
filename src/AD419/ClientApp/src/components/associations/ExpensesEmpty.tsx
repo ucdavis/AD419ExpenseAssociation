573cfef935d083b012f357113a679f2c7cbdeb47
Scott Kirkland <srkirkland@gmail.com>
2020-08-05 15:00:38 -0700

    blind case update

--- src/AD419/ClientApp/src/components/associations/ExpensesEmpty.tsx
+++ src/AD419/ClientApp/src/components/associations/ExpensesEmpty.tsx
@@ -6,20 +6,25 @@ interface Props {
 }
 
 export default function ExpensesEmpty(props: Props): JSX.Element {
-  // Cal -- this being shown depends on what is selected in the expense grouping (show associated only, unassoc only, or both)
-  // so we should customize the message for each or pick a generic one.  I pass in the grouping in the props is case we want to use it
-  if (true) {
-  return (
+  const { showAssociated, showUnassociated } = props.expenseGrouping;
+  if (showUnassociated && !showAssociated) {
+    // if we are showing unassociated expenses only
+    return (
       <div>
-      <p className="text-center pt-2">Looks like there are no more unassociated items in this department <br/>
-      Maybe it's time for an ice cream?
-      </p>
-    </div>
-    ); } 
-    
-    else { return (
+        <p className='text-center pt-2'>
+          Looks like there are no more unassociated items in this department{' '}
+          <br />
+          Maybe it's time for an ice cream?
+        </p>
+      </div>
+    );
+  } else {
+    return (
       <div>
-      <p className="text-center mt-2">There is nothing to display with these parameters</p>
-    </div>
-    ) }
+        <p className='text-center mt-2'>
+          There is nothing to display with these parameters
+        </p>
+      </div>
+    );
+  }
 }
